48619e45a12954f18cc3ec52fc6fbde7c6095222
Dzmitry Bryt <bdv212@yandex.ru>
2020-02-28 16:04:53 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  let arrayCopy = [...matrix];
+
+  for (let i = 0; i < arrayCopy.length; i++) {
+    if (i % 2 === 1) arrayCopy[i].reverse();
+  };
+
+  return arrayCopy.flat();
 }
